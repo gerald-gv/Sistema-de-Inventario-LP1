2ca45f44045dc3ac30f4dcccd6c74811d78a303d
David Carmona <davidc.carmonac@gmail.com>
2025-12-14 02:37:28 -0500

    Implementacion de vista proveedores y Controlador para el proveedor

---
+++ src/main/java/controlador/ControladorProveedor.java
@@ -0,0 +1,64 @@
+package controlador;
+
+import java.io.IOException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+import modelo.Proveedor;
+import modeloDAO.ProveedorDAO;
+
+@WebServlet(name = "ControladorProveedor", urlPatterns = {"/ControladorProveedor"})
+public class ControladorProveedor extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    ProveedorDAO dao = new ProveedorDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String accion = request.getParameter("accion");
+
+        if ("eliminar".equals(accion)) {
+            int id = Integer.parseInt(request.getParameter("id"));
+            dao.eliminar(id);
+        }
+
+        response.sendRedirect(request.getContextPath() + "/layout/dashboard.jsp?view=proveedores");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String accion = request.getParameter("accion");
+
+        if ("guardar".equals(accion)) {
+
+            Proveedor p = new Proveedor();
+            p.setNombre(request.getParameter("nombre"));
+            p.setRuc(request.getParameter("ruc"));
+            p.setTelefono(request.getParameter("telefono"));
+            p.setDireccion(request.getParameter("direccion"));
+            p.setEmail(request.getParameter("email"));
+
+            dao.add(p);
+        }
+
+        if ("actualizar".equals(accion)) {
+            Proveedor p = new Proveedor();
+            p.setId(Integer.parseInt(request.getParameter("id")));
+            p.setNombre(request.getParameter("nombre"));
+            p.setRuc(request.getParameter("ruc"));
+            p.setTelefono(request.getParameter("telefono"));
+            p.setDireccion(request.getParameter("direccion"));
+            p.setEmail(request.getParameter("email"));
+
+
+            dao.edit(p);
+        }
+
+        response.sendRedirect(request.getContextPath() + "/layout/dashboard.jsp?view=proveedores");
+    }
+}
--- src/main/java/modeloDAO/ProveedorDAO.java
+++ src/main/java/modeloDAO/ProveedorDAO.java
@@ -2,6 +2,7 @@ package modeloDAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,26 +53,108 @@ public class ProveedorDAO implements CRUD<Proveedor>{
 
 	@Override
 	public Proveedor list(int id) {
-		// TODO Auto-generated method stub
-		return null;
+        Proveedor p = new Proveedor();
+        /*
+        String sql = "SELECT u.id_usuario, u.username, u.user_email, u.password, "
+                   + "r.id_rol, r.nombre AS nombreRol "
+                   + "FROM usuarios u "
+                   + "JOIN roles r ON u.id_rol = r.id_rol "
+                   + "WHERE u.id_usuario=?";
+	*/
+        String sql = """
+        			SELECT * FROM proveedores
+        			WHERE id_proveedor=?
+        		""";
+        
+        try {
+            Connection con = Conexion.Conectar();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                p.setId(rs.getInt("id_usuario"));
+                p.setNombre(rs.getString("company_name"));
+				p.setRuc(rs.getString("ruc"));
+				p.setTelefono(rs.getString("telefono"));
+				p.setDireccion(rs.getString("direccion"));
+				p.setEmail(rs.getString("email"));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error al obtener usuario: " + e);
+        }
+
+        return p;
 	}
 
 	@Override
-	public boolean add(Proveedor obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean add(Proveedor p) {
+        String sql = "INSERT INTO proveedores(company_name, ruc, telefono, direccion, email) VALUES(?,?,?,?,?)";
+
+        try {
+            Connection con = Conexion.Conectar();
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, p.getNombre());
+            ps.setString(2, p.getRuc());
+            ps.setString(3, p.getTelefono());
+            ps.setString(4, p.getDireccion());
+            ps.setString(5, p.getEmail());
+
+            ps.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error al agregar usuario: " + e);
+        }
+
+        return false;
 	}
 
 	@Override
-	public boolean edit(Proveedor obj) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean edit(Proveedor p) {
+        String sql = "UPDATE proveedores SET company_name=?, ruc=?, telefono=?, direccion=?, email=? WHERE id_proveedor=?";
+
+        try {
+            Connection con = Conexion.Conectar();
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, p.getNombre());
+            ps.setString(2, p.getRuc());
+            ps.setString(3, p.getTelefono());
+            ps.setString(4, p.getDireccion());
+            ps.setString(5, p.getEmail());
+            ps.setInt(6, p.getId());
+
+            ps.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error al editar usuario: " + e);
+        }
+
+        return false;
 	}
 
 	@Override
 	public boolean eliminar(int id) {
-		// TODO Auto-generated method stub
-		return false;
+        String sql = "DELETE FROM proveedores WHERE id_proveedor=?";
+
+        try {
+            Connection con = Conexion.Conectar();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error al eliminar usuario: " + e);
+        }
+
+        return false;
 	}
 
 }
